f2c78a82b08c4a56f3e759a394f4976c1d9ce8c5
Kirill-2019 <48190427+Kirill-2019@users.noreply.github.com>
2019-08-09 02:34:24 +0300

    1

--- src/main/java/first.java
+++ src/main/java/first.java
@@ -1,14 +1,38 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class first {
    private static WebDriver WD;
+
+
    public static void main(String[] args){
 
 
       WD = new ChromeDriver();
+
+      WD.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+      WD.get("http://localhost/litecart/admin/");
+
+      login.login(WD);
+
+      WD.close();
+      WD.quit();
+
+      WD = new InternetExplorerDriver();
+
+      WD.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+      WD.get("http://localhost/litecart/admin/");
+
+      login.login(WD);
+
+      WD.close();
+      WD.quit();
+
+      WD = new FirefoxDriver();
 
       WD.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
       WD.get("http://localhost/litecart/admin/");
